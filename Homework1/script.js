b5a6143af13fcb10da4c8b136b7b208dd0d54257
Kornil777 <kornilovsanya@yandex.ru>
2025-08-17 22:41:24 +0500

    2 задания JS

---
+++ Homework1/script.js
@@ -0,0 +1,14 @@
+// Задание №1
+let password = 'пароль';
+let answer = prompt("Введите пароль");
+if (answer === "пароль") {alert("Пароль введен верно");
+} else {alert("Пароль введен неправильно");
+    
+}
+
+// Задача№2
+let c = 5;
+if (c>0 && c<10) {console.log("Верно");  
+} else {console.log("Неверно");
+    
+}
