d42ff9eef6988e913e5a719eefd62bb41db63626
Kornil777 <kornilovsanya@yandex.ru>
2025-08-20 23:08:43 +0500

    Исправление ДЗ №3

--- Homework1/script.js
+++ Homework1/script.js
@@ -1,20 +1,20 @@
-// // Задание №1
+// // // Задание №1
 let password = "пароль";
 let answer = prompt("Введите пароль");
-if (answer === "пароль") {
+if (answer === password) {
   alert("Пароль введен верно");
 } else {
   alert("Пароль введен неправильно");
 }
 
-// // Задача№2
+// // // Задача№2
 let c = 5;
 if (c > 0 && c < 10) {
   console.log("Верно");
 } else {
   console.log("Неверно");
 }
-// Задача №3
+// // Задача №3
 let e = 50;
 let d = 101;
 if (e > 100 || d > 100) {
@@ -23,7 +23,7 @@ if (e > 100 || d > 100) {
   console.log("Неверно");
 }
 
-// задача №4
+// // задача №4
 let a = "2";
 let b = "3";
 alert(+a + +b);
@@ -32,43 +32,21 @@ alert(+a + +b);
 let monthNumber = Number(prompt("введите номер месяца"));
 
 switch (monthNumber) {
-  case 1:
+  case 1: case 2: case 12:
     alert("Зима");
     break;
-  case 2:
-    alert("Зима");
-    break;
-  case 3:
+  case 3: case 4: case 5:
     alert("Весна");
     break;
-  case 4:
-    alert("Весна");
-    break;
-  case "5":
-    alert("Весна");
-    break;
-  case 6:
+  case 6: case 7: case 8:
     alert("Лето");
     break;
-  case 7:
-    alert("Лето");
-    break;
-  case 8:
-    alert("Лето");
-    break;
-  case 9:
+  case 9: case 10: case 11:
     alert("Осень");
-    break;
-  case 10:
-    alert("Осень");
-    break;
-  case 11:
-    alert("Осень");
-    break;
-  case 12:
-    alert("Зима");
     break;
 
   default:
-    alert("номер месяца не может быть больше 12");
+    alert("не верно введено число месяца");
+    if (0<monthNumber<=13){alert("Число месяца должно быть от 1 до 13")};
 }
+
