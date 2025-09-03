872ad1052228c324d9a9ddcd658e80a31f16b61a
Kornil777 <kornilovsanya@yandex.ru>
2025-09-03 23:14:18 +0500

    Игра №2

--- Homework1/PlayGame.js
+++ Homework1/PlayGame.js
@@ -1,3 +1,5 @@
+
+// 1-я игра Случайное число
 function startGame1() {
   const randomNumber = Math.floor(Math.random() * 100) + 1;
   console.log(randomNumber);
@@ -19,3 +21,71 @@ function startGame1() {
     }
   } while (userNumber != randomNumber);
 }
+
+// 2-я игра Простая арифметика
+function Game2task() {
+            const operations = ['+', '-', '*', '/'];
+            const operation = operations[Math.floor(Math.random() * operations.length)];
+            
+            let num1, num2, answer;
+            
+            switch(operation) {
+                case '+':
+                    num1 = Math.floor(Math.random() * 20) + 1;
+                    num2 = Math.floor(Math.random() * 20) + 1;
+                    answer = num1 + num2;
+                    break;
+                    
+                case '-':
+                    num1 = Math.floor(Math.random() * 20) + 10;
+                    num2 = Math.floor(Math.random() * num1) + 1;
+                    answer = num1 - num2;
+                    break;
+                    
+                case '*':
+                    num1 = Math.floor(Math.random() * 10) + 1;
+                    num2 = Math.floor(Math.random() * 10) + 1;
+                    answer = num1 * num2;
+                    break;
+                    
+                case '/':
+                    num2 = Math.floor(Math.random() * 10) + 1;
+                    answer = Math.floor(Math.random() * 10) + 1;
+                    num1 = num2 * answer; 
+                    break;
+            }
+            
+            return {
+                question: `${num1} ${operation} ${num2}`,
+                answer: answer
+            };
+        }
+
+        function startGame2() {
+            const problem = Game2task();
+            const userAnswer = prompt(`Решите пример: ${problem.question}`);
+            
+            if (userAnswer === null) {
+                alert('Игра окончена!');
+                return;
+            }
+            
+            const numericAnswer = parseFloat(userAnswer);
+            
+            if (isNaN(numericAnswer)) {
+                alert('Пожалуйста, введите число!');
+            } else if (Math.abs(numericAnswer - problem.answer) < 0.001) {
+                alert('Правильно! Молодец!');
+            } else {
+                alert(`Неправильно. Правильный ответ: ${problem.answer}`);
+            }
+            
+            
+            if (confirm('Хотите сыграть еще раз?')) {
+                startGame2();
+            } else {
+                alert('Спасибо за игру!');
+            }
+        }
+
+      
